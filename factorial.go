94818276877d3a529c84df925497079494e42816
Kalyan <140071276+Kalyanrishi@users.noreply.github.com>
2024-11-21 11:37:45 +0530

    Add files via upload

---
+++ factorial.go
@@ -0,0 +1,20 @@
+//  1) Write a Golang program for calculating the factorial of a given number.
+
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	var num int
+	fmt.Println("Enter the value of num: ")
+	fmt.Scan(&num)
+	result := factorial(num)
+	fmt.Printf("Factorial of %d is %d\n", num, result)
+}
